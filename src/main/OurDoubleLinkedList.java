3977452c1cb68c44a0d04ef5d4c893154a65bcb0
Bogdan <bogdan.garabadzhiu@yandex.ru>
2020-11-10 09:24:05 +0200

    OurDoubleLinkedList

--- src/main/OurDoubleLinkedList.java
+++ src/main/OurDoubleLinkedList.java
@@ -2,6 +2,10 @@ package main;
 
 public class OurDoubleLinkedList implements OurList{
 
+    private Element first = null;
+    private Element last = null;
+    private int size = 0;
+
     private class Element {
         Integer value;
         Element next = null;
@@ -12,103 +16,148 @@ public class OurDoubleLinkedList implements OurList{
         }
     }
 
-    private Element first = null;
-    private Element last = null;
-    private int size = 0;
-
     @Override
     public void add(Integer e) {
         Element newElement = new Element(e);
         if (first == null) {
-            first = newElement;
-            last = first;
+            first = last = newElement;
+            first.prev = null;
+            last.next = null;
         } else {
             last.next = newElement;
             newElement.prev = last;
             last = newElement;
+            last.next = null;
         }
         size++;
     }
 
     @Override
     public Integer get(int index) {
-        if (first == null || index < 0) {
-            return null;
-        }
-        Element tmp = null;
-        if (index < size() / 2) {
-            tmp = first;
-            for (int i = 0; i < index; i++) {
+        {
+            if (first == null || index < 0) {
+                return null;
+            }
+            Element tmp = null;
+            if (index < size() / 2) {
+                tmp = first;
+                for (int i = 0; i < index; i++) {
+                    try {
+                        tmp = tmp.next;
+                    } catch (NullPointerException e) {
+                        return null;
+                    }
+                }
                 try {
-                    tmp = tmp.next;
+                    return tmp.value;
                 } catch (NullPointerException e) {
                     return null;
                 }
-            }
-            try {
-                return tmp.value;
-            } catch (NullPointerException e) {
-                return null;
-            }
-        } else {
-            tmp = last;
-            for (int i = size() - 1; i > index; i--) {
+            } else {
+                tmp = last;
+                for (int i = size() - 1; i > index; i--) {
+                    try {
+                        tmp = tmp.prev;
+                    } catch (NullPointerException e) {
+                        return null;
+                    }
+                }
                 try {
-                    tmp = tmp.prev;
+                    return tmp.value;
                 } catch (NullPointerException e) {
                     return null;
                 }
-            }
-            try {
-                return tmp.value;
-            } catch (NullPointerException e) {
-                return null;
             }
         }
     }
 
     @Override
     public void set(int index, Integer e) {
-
+        if (first == null || index < 0) {
+            return;
+        }
+        if (index < size / 2) {
+            Element tmp = first;
+            for (int i = 0; i < index; i++) {
+                tmp = tmp.next;
+            }
+            tmp.value = e;
+        } else {
+            Element tmp = last;
+            for (int j = size; j > index; j--) {
+                tmp = tmp.prev;
+            }
+            tmp.value = e;
+        }
     }
 
-    @Override
     public void remove(int index) {
 
+        if (first == null || index < 0) {
+            return;
+        }
+        if (index < size / 2) {
+            for (int i = 0; i <= index; i++) {
+                first = first.next;
+            }
+            first.prev = null;
+        } else {
+            for (int j = size; j > index; j--) {
+                last = last.prev;
+            }
+            last.next = null;
+        }
+        size--;
     }
 
     @Override
     public void clear() {
-
+        first = null;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int indexOf(Integer e) {
-        return 0;
+        int index = 0;
+        if (e == null) {
+            for (Element tmp = first; tmp != null; tmp = tmp.next) {
+                if (tmp.value == null)
+                    return index;
+                index++;
+            }
+        } else {
+            for (Element tmp = first; tmp != null; tmp = tmp.next) {
+                if (e.equals(tmp.value))
+                    return index;
+                index++;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Integer e) {
-        return 0;
+        int i = size()-1;
+        Element tmp = last;
+        while (tmp != null && !tmp.value.equals(e))
+        {
+            tmp = tmp.prev;
+            i--;
+        }
+        if (tmp == null)
+        {
+            return -1;
+        } else {
+            return i;
+        }
     }
 
     @Override
     public int size() {
-//        if (first == null) {
-//            return 0;
-//        }
-//        Element tmp = first;
-//        int counter = 1;
-//        while (tmp.next != null) {
-//            tmp = tmp.next;
-//            counter++;
-//        }
-//        return counter;
         return size;
     }
 }
